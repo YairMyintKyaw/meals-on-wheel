17b1b8080f0cb73a18c77641878bbbff593c5479
Ye Myint Kyaw <yemyintkyaw241220@gmail.com>
2024-03-08 00:31:23 +0630

    auth class created

---
+++ src/util/auth.ts
@@ -0,0 +1,50 @@
+import axios from "axios";
+
+export class Auth{
+    name:string;
+    email:string;
+    _token:string;
+    _apiUrl:string = "http://127.0.0.1:8000/api/";
+    constructor(name:string = "John", email:string = "john6@gmail.com"){
+        this.name= name;
+        this.email = email;
+    }
+    register(password:string){
+        axios.post(this._apiUrl+"register",
+            {
+                "name" : this.name,
+                "email" : this.email,
+                "password" : password
+            },
+            {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response=>{
+                console.log(response)
+                this._token = response.data.token
+            })
+            .catch(error=>console.log(error))
+    }
+
+    login(password:string){
+        axios.post(this._apiUrl+"login",
+            {
+                "email":this.email,
+                "password":password
+            },
+            {
+                headers: {
+                    Authorization: `Bearer ${this._token}`
+                }
+            }
+        )
+    }
+
+    logout(){
+        axios.post(this._apiUrl+"logout");  
+    }
+
+
+}
