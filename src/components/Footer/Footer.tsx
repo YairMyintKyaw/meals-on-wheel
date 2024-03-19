5fcaae5eb645963ffdbef5d8a6c505caf2b2b796
Ye Myint Kyaw <yemyintkyaw241220@gmail.com>
2024-03-20 02:57:32 +0630

    update footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,12 +5,13 @@ import { FaFacebookF, FaInstagram, FaLinkedin, FaTwitter, FaYoutube } from "reac
 
 const Footer = () => {
     return (
-        <footer className="py-[91px] px-[20px] max-w-[1512px] mx-auto w-full">
-            <div className="flex flex-wrap items-start justify-center gap-10 gap-y-12 w-full">
+        <footer className="pt-[91px] pb-[31px] px-[20px] max-w-[1512px] mx-auto w-full">
+            <div className="flex flex-wrap flex-col items-center text-center md:text-start md:items-start md:flex-row justify-center gap-10 md:gap-y-12 w-full mb-24">
                 <Image width={163} height={52} Img={Logo} className="w-[163px]" />
+                <div className="w-full lg:hidden"></div>
                 <div>
                     <h2 className="text-xl font-bold mb-[17.14px]">Learn More</h2>
-                    <ul className="text-sm font-medium">
+                    <ul className="text-neutral-800 hover:text-black text-sm font-medium">
                         <li><Link to={""}>About MerryMeal</Link></li>
                         <li><Link to={""}>Our impact</Link></li>
                         <li><Link to={""}>Meet our Team</Link></li>
@@ -20,7 +21,7 @@ const Footer = () => {
                 </div>
                 <div>
                     <h2 className="text-xl font-bold mb-[17.14px]">Roles</h2>
-                    <ul className="text-sm font-medium">
+                    <ul className="text-neutral-800 hover:text-black text-sm font-medium">
                         <li><Link to={""}>Become a Donator</Link></li>
                         <li><Link to={""}>Become a Volunteer</Link></li>
                         <li><Link to={""}>Become a Partner</Link></li>
@@ -28,14 +29,14 @@ const Footer = () => {
                     </ul>
                 </div>
                 <div>
-                    <h2 className="text-xl font-bold mb-[17.14px]">Learn More</h2>
-                    <ul className="text-sm font-medium">
+                    <h2 className="text-xl font-bold mb-[17.14px]">Contact Us</h2>
+                    <ul className="text-neutral-800 hover:text-black text-sm font-medium">
                         <li><Link to={""}>Hot-line Number : 123-456-7890</Link></li>
                     </ul>
                 </div>
                 <div>
                     <h2 className="text-xl font-bold mb-[17.14px]">Learn More</h2>
-                    <ul className="text-sm font-medium flex justify-center gap-3">
+                    <ul className="text-neutral-800 hover:text-black text-sm font-medium flex justify-center gap-3">
                         <li><Link to={""}><FaFacebookF /></Link></li>
                         <li><Link to={""}><FaInstagram /></Link></li>
                         <li><Link to={""}><FaTwitter /></Link></li>
@@ -44,7 +45,8 @@ const Footer = () => {
                     </ul>
                 </div>
             </div>
-
+            <hr />
+            <p className="text-xs text-center text-neutral-800 mt-[31px]">© MerryOnWheel 2023-2028 | All Rights Reserved</p>
         </footer>
     )
 }
